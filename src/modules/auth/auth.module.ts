e578273f434f855f26d703b50611e6c7841bd67f
Asela Maduwantha <112416067+asela-maduwantha@users.noreply.github.com>
2024-10-25 23:49:53 +0530

    jet secret key issue fixed

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports:[UsersModule],
-  providers: [AuthService,JwtService],
-  controllers:[AuthController]
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'eventNexus', 
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }, 
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
