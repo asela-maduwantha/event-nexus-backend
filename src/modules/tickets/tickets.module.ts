f5b7c4edd6f0dc9de5dac5406e7e25f38008bd07
Asela Maduwantha <112416067+asela-maduwantha@users.noreply.github.com>
2024-10-25 23:10:51 +0530

    ticket-service and controller

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { Ticket, TicketSchema } from './schemas/ticket.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Ticket.name, schema: TicketSchema }]) 
+  ],
   providers: [TicketsService],
   controllers: [TicketsController]
 })
--- src/modules/tickets/tickets.service.ts
+++ src/modules/tickets/tickets.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Ticket, TicketDocument } from './schemas/ticket.schema';
+import { Model } from 'mongoose';
+import { CreateTicketDto } from './dto/create-ticket.dto';
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService {
+    constructor(@InjectModel(Ticket.name) private TicketModel: Model<TicketDocument>){}
+
+    //create tickets for event
+    async createTicket(createTicketDto: CreateTicketDto): Promise<TicketDocument> {
+        const { type, eventId } = createTicketDto;
+
+        const existingTicket = await this.TicketModel.findOne({ type, eventId });
+        if (existingTicket) {
+            throw new BadRequestException(`Ticket of type '${type}' already exists for this event.`);
+        }
+
+        const newTicket = new this.TicketModel(createTicketDto);
+        return newTicket.save();
+    }
+
+    //get tickets by event Id
+    async getTicketsByEventId(eventId: string): Promise<TicketDocument[]> {
+        const tickets = await this.TicketModel.find({ event: eventId }).exec();
+        
+        if (!tickets || tickets.length === 0) {
+            throw new NotFoundException(`No tickets found for event with ID ${eventId}`);
+        }
+
+        return tickets;
+    }
+}
